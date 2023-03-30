2bc865cdc47226a71d2b854895603ee4f5f60aee
Hassenosman <hassanosman200@gmail.com>
2023-03-30 09:57:01 -0500

    newfile

---
+++ class/components/MovieList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Movie from "./Movie";
+
+const MovieList = (props) => {
+  return (
+    <div>
+      {props.movies.map((movie) => {
+        return (<Movie movie={movie} key={movie.id} />
+        )
+      })}
+    </div>
+  );
+};
+
+export default MovieList;
---
+++ class/components/Review.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Review = (props) => {
+  return (
+    <div>
+      <li>
+      {props.review.review}
+      </li>
+        
+    </div>
+  );
+};
+
+export default Review;
